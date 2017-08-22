01e449428ad9b9a4254dd0e85f88711bf284df67
Edward Plato <edplatomail@gmail.com>
2017-08-22 08:43:34 -0700

    Pass tests for getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,32 @@
 // If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
+// var getElementsByClassNa me = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+    // create empty array
+    var elementsArray = [];
+
+    //searching function (node/element)
+    var searchElements = function(element) {
+        // check new array for matching className
+        if(element.classList && element.classList.contains(className)) {
+        // save element in array
+          elementsArray.push(element);
+        }
+
+        //check if there are children
+        if (element.hasChildNodes()) {
+            // if so, loop through and then
+            for (var i = 0; i < element.childNodes.length; i++) {
+                // recursively call searchingFunction on the child of the element
+                searchElements(element.childNodes[i]);
+            }
+        }
+    };
+    // searchingFunction(document.body);
+    searchElements(document.body);
+    return elementsArray;
+    // return elementsArray
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // check for the individual base cases
+   // undefined
+   if( obj === undefined) {
+    return 'undefined';
+   // null
+   } else if(obj === null) {
+    return 'null';
+   // boolean
+   } else if (typeof obj === 'boolean'){
+      return obj === true ? "true" : "false";
+    //number
+   } else if (typeof obj === 'number'){
+      return '' + obj;
+    //string
+   } else if (typeof obj === 'string'){
+      return '"' + obj + '"';
+    //array
+   } else if(Array.isArray(obj)){
+      return '[' + obj.reduce(function(acc, val){
+        return acc.concat(stringifyJSON(val));
+      }, []) + ']';
+    //object
+   } else if (typeof obj === 'object') {
+       var result = '';
+       for (var key in obj) {
+           if (key === 'undefined') {
+               return '{}';
+           }
+           result += stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+       }
+       return '{' + result.slice(0, result.length - 1) + '}';
+   }
 };
